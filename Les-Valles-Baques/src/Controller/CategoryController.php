12780189143d4f3afa7c55b597bb0f744a2b29d7
v1c3nt <vince.78@free.fr>
2018-09-14 13:10:43 +0200

    for test

--- Les-Valles-Baques/src/Controller/CategoryController.php
+++ Les-Valles-Baques/src/Controller/CategoryController.php
@@ -8,16 +8,56 @@ use App\Repository\CategoryRepository;
 
 class CategoryController extends AbstractController
 {
+
     /**
      * @Route("/category", name="category")
      */
     public function index(CategoryRepository $category)
     {
-        $category = $category->findAll();
+        $treeCategory = [];
+        $categorys = $category->findAll();
 
+        foreach ($categorys as $category) {
+            dump($category->getChildren());
+        }
+        /**
+         *! teste avec twig
+         * foreach ($categorys as $category) {
+            dump($category);
+            do {
+                if ( null === $category->getParent() ) {
+                    $parent = null;
+                } else {
+                $parent=1;
+                foreach ($category as $subcategorys ) {
+                    $treeCategory[] = $subcategorys;
+                    dump('while');
+                    dump($subcategorys);die();
+
+                        $parent = $subcategorys->getParent();
+                        $category = $subcategorys;
+                        return $category;
+                    }
+                }
+            }
+            while (null !== $parent);
+
+
+                $treeCategory[] = $category;
+            }
+                dump('dd');
+        
+        dump($treeCategory);
+        exit;
+         */
         return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
-            'categories'=> $category,
+            'categories' => $categorys,
         ]);
+    }
+
+    public function deepdown($cat)
+    {
+
     }
 }
--- Les-Valles-Baques/src/Entity/Category.php
+++ Les-Valles-Baques/src/Entity/Category.php
@@ -28,14 +28,23 @@ class Category
      */
     private $quizzs;
 
+
     /**
-     * @ORM\Column(type="integer")
+     * Many Categories have One Category.
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy="children")
+     * @ORM\JoinColumn(name="parent_id", referencedColumnName="id")
      */
-    private $idParent;
+    private $parent;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Category", mappedBy="parent")
+     */
+    private $children;
 
     public function __construct()
     {
         $this->quizzs = new ArrayCollection();
+        $this->children = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -86,21 +95,59 @@ class Category
         return $this;
     }
 
-  
-    public function getIdParent(): ?int
+    public function __toString()
     {
-        return $this->idParent;
+        return $this->getName();
     }
 
-    public function setIdParent(int $idParent): self
+    /**
+     * @return Collection|Category[]
+     */
+    public function getChildren(): Collection
     {
-        $this->idParent = $idParent;
+        return $this->children;
+    }
+
+    public function addChild(Category $child): self
+    {
+        if (!$this->children->contains($child)) {
+            $this->children[] = $child;
+            $child->setParent($this);
+        }
 
         return $this;
     }
 
-    public function __toString()
+    public function removeChild(Category $child): self
     {
-        return $this->getName();
+        if ($this->children->contains($child)) {
+            $this->children->removeElement($child);
+            // set the owning side to null (unless already changed)
+            if ($child->getParent() === $this) {
+                $child->setParent(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get many Categories have One Category.
+     */ 
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * Set many Categories have One Category.
+     *
+     * @return  self
+     */ 
+    public function setParent($parent)
+    {
+        $this->parent = $parent;
+
+        return $this;
     }
 }
