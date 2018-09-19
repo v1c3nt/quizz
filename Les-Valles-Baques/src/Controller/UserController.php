fa510905d0aab085d01451450f9c8a39e7acea63
v1c3nt <vince.78@free.fr>
2018-09-19 10:56:17 +0200

    dynamic profil myQuizzs

--- Les-Valles-Baques/src/Controller/UserController.php
+++ Les-Valles-Baques/src/Controller/UserController.php
@@ -4,20 +4,26 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Repository\QuizzRepository;
+use App\Repository\UserCrewRepository;
 class UserController extends AbstractController
 {
     /**
      * @Route("/{username}/profile", name="user_profil")
      */
-    public function showProfil()
+    public function showProfil(QuizzRepository $quizzes, UserCrewRepository $uCrews)
     {
         $user = $this->getUser();
-
+        $myQuizzes = $quizzes->findByAuthor($user);
+        $myCrews = $uCrews->findByUser($user);
         dump($user);
+        dump($myQuizzes);
+        dump($myCrews);
 
         return $this->render('user/index.html.twig', [
-            'user' => $user
+            'user' => $user,
+            'myQuizzes' => $myQuizzes,
+            'myCrews' => $myCrews,
             
         ]);
     }
--- Les-Valles-Baques/src/Entity/Quizz.php
+++ Les-Valles-Baques/src/Entity/Quizz.php
@@ -281,4 +281,6 @@ class Quizz
     {
         return $this->getTitle();
     }
+
+
 }
