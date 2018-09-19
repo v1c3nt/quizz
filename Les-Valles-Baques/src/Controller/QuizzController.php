8eb28827c87d8daabd8223b42e0ab1dbcebe1400
Virginia <virginia.atenza@gmail.com>
2018-09-19 09:11:15 +0200

    commit before pull new version

--- Les-Valles-Baques/src/Controller/QuizzController.php
+++ Les-Valles-Baques/src/Controller/QuizzController.php
@@ -51,17 +51,6 @@ class QuizzController extends AbstractController
      */
     public function new(Request $request, ObjectManager $manager)
     {
-<<<<<<< HEAD
-        $quizz = new Quizz();
-        $question = new Question();
-    
-        $form = $this->createForm(QuizzType::class, $quizz);
-        
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $manager->persist($quizz);
-=======
         $user = $this->getUser();
         $quizz = new Quizz();
 
@@ -108,22 +97,19 @@ class QuizzController extends AbstractController
 
         $form = $this->createForm(QuestionType::class, $question);
         $form->handleRequest($request);
-     
+        
         //? je crée une variable pour compter le nombre de question créées
         $nbr++;
         //c'est la avant le if
         if ($form->isSubmitted() && $form->isValid()) {
-
             $question->setQuizz($quizz);
             $question->setErrore(0);
 
             $manager->persist($question);
->>>>>>> a4bbf86912ba4f2262580a673faa7b324eab670c
 
             $manager->flush();
 
             if ($nbr < 10) {
-
                 $question = new Question();
                 $form = $this->createForm(QuestionType::class, $question);
 
@@ -139,18 +125,10 @@ class QuizzController extends AbstractController
             ]);
         }
 
-<<<<<<< HEAD
-        return $this->render('quizz/new.html.twig', [
-            'form'=>$form->createView(),
-            'questions'=>$questions,
-            'quizz'=>$quizz,
-=======
         return $this->render('quizz/newsQuestions.html.twig', [
             'form' => $form->createView(),
             'quizz' => $quizz,
             'nbr' => $nbr,
->>>>>>> a4bbf86912ba4f2262580a673faa7b324eab670c
         ]);
     }
 }
-
--- Les-Valles-Baques/src/Form/QuestionType.php
+++ Les-Valles-Baques/src/Form/QuestionType.php
@@ -7,7 +7,6 @@ use App\Entity\Question;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use App\Form\QuizzType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
