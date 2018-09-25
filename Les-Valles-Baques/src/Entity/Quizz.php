6a47451aa04d63261cc59a81196baa3fdf3ee9b4
v1c3nt <vince.78@free.fr>
2018-09-25 17:31:39 +0200

    add entitys for optimisatoin quizzes request

--- Les-Valles-Baques/src/Entity/Quizz.php
+++ Les-Valles-Baques/src/Entity/Quizz.php
@@ -85,6 +85,16 @@ class Quizz
      */
     private $level;
 
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $nbrLikes;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $avgScore;
+
     public function __construct()
     {
         $this->questions = new ArrayCollection();
@@ -294,6 +304,30 @@ class Quizz
     public function __toString()
     {
         return $this->getTitle();
+    }
+
+    public function getNbrLikes(): ?int
+    {
+        return $this->nbrLikes;
+    }
+
+    public function setNbrLikes(?int $nbrLikes): self
+    {
+        $this->nbrLikes = $nbrLikes;
+
+        return $this;
+    }
+
+    public function getAvgScore(): ?int
+    {
+        return $this->avgScore;
+    }
+
+    public function setAvgScore(?int $avgScore): self
+    {
+        $this->avgScore = $avgScore;
+
+        return $this;
     }
 
 
